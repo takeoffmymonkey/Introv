9ddba1daef581a01beb90ab637d2a88256be59cb
takeoffmymonkey <galukhin@gmail.com>
2019-05-15 15:55:37 +0300

    Navigation Drawer

--- app/src/main/java/com/galukhin/introv/view/MainActivity.java
+++ app/src/main/java/com/galukhin/introv/view/MainActivity.java
@@ -1,15 +1,20 @@
 package com.galukhin.introv.view;
 
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.galukhin.introv.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +22,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        /* --- PAGER --- */
+        /* ------ NAVIGATION DRAWER ------ */
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string
+                .navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        /* ------ PAGER ------ */
         ViewPager vpPager = findViewById(R.id.vpPager);
         vpPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-
             @Override
             public void onPageSelected(int position) {
             }
@@ -42,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    /* --- MENU --- */
+    /* ------ MENU ------ */
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
@@ -57,6 +75,43 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    /* ------ NAVIGATION DRAWER ------ */
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+// Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+        } else if (id == R.id.nav_import_export) {
+
+        } else if (id == R.id.nav_templates) {
+
+        } else if (id == R.id.nav_settings) {
+
+        } else if (id == R.id.nav_contact) {
+
+        } else if (id == R.id.nav_about) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    /*Handle Back Press For Drawer*/
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 }
